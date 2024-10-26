659e3a654ab51325f77443fa34233760fa6d8455
unknown <ryanchehab20@gmail.com>
2024-10-26 18:59:18 +0300

    corrected event listner line 67

--- scripts/game.js
+++ scripts/game.js
@@ -1,19 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+
 // hard coded options
 var words = [
-    "apple", "banana", "cherry", "dog", "elephant", "flower", "grape", "house", "iguana", "jacket",
-    "kite", "lemon", "mountain", "notebook", "orange", "pencil", "queen", "rabbit", "sun", "turtle",
-    "umbrella", "violin", "whale", "xylophone", "yogurt", "zebra", "airplane", "balloon", "cloud", "dinosaur",
-    "eagle", "fish", "giraffe", "helicopter", "island", "jelly", "kangaroo", "lamp", "moon", "nest",
-    "octopus", "paint", "quilt", "robot", "star", "train", "unicorn", "vase", "wind", "x-ray",
-    "yogurt", "anchor", "bicycle", "cactus", "drum", "envelope", "feather", "ghost", "honey", "ice",
-    "jigsaw", "koala", "leaf", "mushroom", "necklace", "olive", "pancake", "quokka", "rainbow", "snow",
-    "telescope", "acorn", "bell", "cupcake", "dove", "espresso", "flamingo", "garden", "heart", "igloo",
-    "jackal", "kiwi", "lollipop", "mermaid", "narwhal", "otter", "pineapple", "quiver", "raccoon", "snowflake",
-    "toucan", "vanilla", "wolverine", "xerus", "yak", "zest", "avocado", "beetle", "cinnamon", "daffodil"
+    "APPLE", "BANANA", "CHERRY", "DOG", "ELEPHANT", "FLOWER", "GRAPE", "HOUSE", "IGUANA", "JACKET",
+    "KITE", "LEMON", "MOUNTAIN", "NOTEBOOK", "ORANGE", "PENCIL", "QUEEN", "RABBIT", "SUN", "TURTLE",
+    "UMBRELLA", "VIOLIN", "WHALE", "XYLOPHONE", "YOGURT", "ZEBRA", "AIRPLANE", "BALLOON", "CLOUD", "DINOSAUR", 
+    "EAGLE", "FISH", "GIRAFFE", "HELICOPTER", "ISLAND", "JELLY", "KANGAROO", "LAMP", "MOON", "NEST",
+    "OCTOPUS", "PAINT", "QUILT", "ROBOT", "STAR", "TRAIN", "UNICORN", "VASE", "WIND", "X-RAY",
+    "YOGURT", "ANCHOR", "BICYCLE", "CACTUS", "DRUM", "ENVELOPE", "FEATHER", "GHOST", "HONEY", "ICE",
+    "JIGSAW", "KOALA", "LEAF", "MUSHROOM", "NECKLACE", "OLIVE", "PANCAKE", "QUOKKA", "RAINBOW", "SNOW",
+    "TELESCOPE", "ACORN", "BELL", "CUPCAKE", "DOVE", "ESPRESSO", "FLAMINGO", "GARDEN", "HEART", "IGLOO",
+    "JACKAL", "KIWI", "LOLLIPOP", "MERMAID", "NARWHAL", "OTTER", "PINEAPPLE", "QUIVER", "RACCOON", "SNOWFLAKE",
+    "TOUCAN", "VANILLA", "WOLVERINE", "XERUS", "YAK", "ZEST", "AVOCADO", "BEETLE", "CINNAMON", "DAFFODIL"
 ];
 
 var dictionary = {
-    "head": "head.svg",
+    "head": "assets/head.svg",
     "body": "body.svg",
     "left-hand":"left_hand.svg",
     "right-hand":"right-hand.svg",
@@ -27,7 +30,7 @@ function generate(words){
     return words[random];
 }
 // this is the real answere
-target= generate(words)
+let target= generate(words)
 
 // answer div
 function createDiv(target){
@@ -45,50 +48,55 @@ function createDiv(target){
 createDiv(target)
 
 // listens to what the user type
-document.addEventListener('keydown', function(event,body_index){
-    let valid='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    if (valid.includes(event.key)){
-        // add it to the answ
-        let input= event.key;
-    }
-    else{
-        //view part of the body
+// document.addEventListener('keydown', function(event){
+//     let valid='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+//     if (valid.includes(event.key)){
+//         // add it to the answ
+//         let input= event.key;
+//     }
+//     else{
+//         //view part of the body
 
-        // showBody(dictionary,wrongGuesses,order)
-    }
-})
+//         // showBody(dictionary,wrongGuesses,order)
+//     }
+// })
 
-// selects all letters
-let letter = document.querySelectorAll('.letter');
+let letters = document.querySelectorAll('.letter');
 
-// listens to what the user presses on the screen
-letter.forEach(letter=>{
-    letter.addEventListener('click',function(event){
-        clickedDiv = event.target.innerHTML;
-        console.log(clickedDiv)
-        // if
-});
+// Listens to what the user clicks on the screen
+letters.forEach(letter => {
+    letter.addEventListener('click', function(event) {
+        const clickedDiv = event.target.innerHTML; 
+       
+        if (target.includes(clickedDiv)) {
+            console.log(`${clickedDiv} is in the target word.`);
+        } else {
+            console.log(`${clickedDiv} is NOT in the target word.`);
+            showBody(dictionary,wrongGuesses,order)
+        }
+    })
 })
 
 let wrongGuesses = 0;
 var order = ["head", "body", "left-hand", "right-hand", "left-leg", "right-leg"]
 function showBody(dictionary,wrongGuesses,order){
     if(wrongGuesses < order.length){
-        let imgsDiv = document.getElementById('hang');
-        let imgs = createElement('img');
-        bodyPart = order[wrongGuesses]
-        imgs.src = dictionary[bodyPart]
-        imgs.classList.add('bodypart')
 
+        let imgsDiv = document.getElementById('hang');
+        let imgs = document.createElement('img');
+        
+        imgs.src = dictionary[order[wrongGuesses]]
+        imgs.classList.add(order[wrongGuesses])
+
+        imgsDiv.appendChild(imgs)
         wrongGuesses++;
+    }
+    else{
+        console.log("gameOver")
     }
     
         // imgs.sr
 }
 
-function centralJudge(target,input) {
-    console.log(target);
-    console.log(input)
-    // if
-}
-centralJudge(target)
+
+});
